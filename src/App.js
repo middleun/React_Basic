c0d59817a9db20960cb679238c018b42da8606cd
middleun <eunallaco@gmail.com>
2021-04-01 15:51:46 +0900

    210401_react 테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class Greeting extends Component{
+  // component에서 상속받은 greeting
+  // render함수의 내부에 return값은 반드시 하나의 태그로 감싸져야 함!
+  render(){
+    return(
+      <div>
+        <p>Hello Again React</p>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <p>Hello React!</p>
+      <Greeting/>
       
     </div>
   );
